9810e7814cd06fd4eb99bb909a773e48427ba8d9
Pseudo Ninja <jaihieong88@gmail.com>
2019-03-29 23:39:16 -0400

    final

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,9 +18,9 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    // showAll();
-    askUser();
-    connection.end();
+    showAll();
+    
+    
     
 });
 
@@ -31,7 +31,14 @@ function showAll() {
         
         askUser();
         
-    });
+    })
+
+    // connection.query("SELECT * FROM products", function(err, res) {
+        
+    //     console.log(id, quantity);
+    //     console.log(res);
+        
+    // })
 };
 
 function askUser() {
@@ -50,41 +57,85 @@ function askUser() {
             }
         ])
         .then(function(answer){
-            // checkAvailability(answer.id, answer.quantity);
+            checkAvailability(answer.id, answer.quantity);
 
-            console.log(answer.id);
-            console.log(answer.quantity);
+            // console.log(answer.id);
+            // console.log(answer.quantity);
 
-            var query = 'SELECT * FROM products';
-            connection.query(query, function(err, res) {
-            // if (err) throw err;
-            console.log(res);
+            // var query = 'SELECT * FROM products';
+            // connection.query(query, function(err, res) {
+            // // if (err) throw err;
+            // console.log(res);
 
-            // for (var i = 0; i < res.length; i++){
-            //     if (answer.id === res[i].id){
-            //         console.log(res[i]);
-            //     }
-            // }
+            // // for (var i = 0; i < res.length; i++){
+            // //     if (answer.id === res[i].id){
+            // //         console.log(res[i]);
+            // //     }
+            // // }
 
-            })
+            // })
             
         })
+        
 };
 
 function checkAvailability(id, quantity){
     connection.query("SELECT * FROM products",
     function(err, res) {
-        // if (err) throw err;
-        console.log(res);
-
+        if (err) throw err;
+        // iterate through the forloop to find the matching product using its item_id
         for (var i = 0; i < res.length; i++){
-            if (id === res[i].id){
-                console.log(res[i])
+            // when matching id is found, assign it to new variable
+            var selectID = parseInt(res[i].item_id);
+            if (parseInt(id) === selectID){
+                var chosenItem = res[i];
+                if (parseInt(quantity) > chosenItem.stock_qt) {
+                    console.log("NOT ENOUGH IN STOCK, Please try your order again");
+                    console.log("Product selected: " + chosenItem.product_name);
+                    console.log("Quantity selected: " + parseInt(quantity));
+                } else {
+                    // place the order and deduct the ordered quantity from the database for that specific product
+                    // console.log(chosenItem);
+                    console.log("order successfully placed");
+                    console.log("Product selected: " + chosenItem.product_name);
+                    console.log("Quantity selected: " + parseInt(quantity));
+                    updateProduct(chosenItem, quantity);
+                }
+                break;
             }
         }
+        
+
     })
-    // console.log("checking product availability");
-    // console.log(id);
-    // console.log(quantity);
-    
+
 };
+
+function updateProduct(item, quantity) {
+    console.log("===================================================");
+    console.log("Updating stock quantity for: " + item.product_name);
+    // console.log(item, quantity);
+    var query = connection.query(
+      "UPDATE products SET ? WHERE ?",
+      [
+        {
+          stock_qt: parseInt(item.stock_qt) - parseInt(quantity)
+        },
+        {
+          item_id: item.item_id
+        }
+      ],
+      function(err, res) {
+        if (err) throw err;
+        // console.log(res);
+        console.log(res.affectedRows + " products updated!\n");
+        // console.log("new quantity" + item.stock_qt);
+      }
+    );
+    
+    // logs the actual query being run
+    // console.log(query.sql);
+
+    console.log("Total cost for this order: $" + parseInt(item.price) * parseInt(quantity));
+
+    connection.end();
+  }
